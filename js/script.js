068bcd1be475a6a50ad61f37aecde476fea1497f
Filipp Tihonovich <tihonshik@gmail.com>
2018-10-24 02:28:49 +0300

    create some content and fix

--- js/script.js
+++ js/script.js
@@ -7,3 +7,43 @@ menu.onclick = function myFunction() {
         x.className = "topnav";
     }
 }
+
+$(document).ready(function(){
+
+    $("#myTopnav").on("click","a", function (event) {
+
+//отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+//збираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+$(function() {
+
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() != 0) {
+
+            $('#toTop').fadeIn();
+
+        } else {
+
+            $('#toTop').fadeOut();
+
+        }
+
+    });
+
+    $('#toTop').click(function() {
+
+        $('body,html').animate({scrollTop:0},800);
+
+    });
+
+});
